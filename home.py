3bb4a68007bbe0ca9a973747975ad65072fc5dd6
Utpal Mishra <76061500+Utpal-Mishra@users.noreply.github.com>
2021-09-06 12:47:32 +0530

    Add files via upload

---
+++ home.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  5 01:24:27 2021
+
+@author: Utpal
+"""
+
+import streamlit as st
+
+def app():
+    st.write("")
+    st.write("")
+    st.write("")
+    
+    st.title("OMDENA NIGERIA LOCAL CHAPTER 2021")
+    
+    st.write("")
+    st.write("")
+    st.write("")
+    
+    st.header("LOAN ELIGIBILITY/ ECONOMY STATUS OF NIGERIA")
+    
+    st.write("")
+    st.write("")
+    
+    st.write("APPLICATION DESCRIPTION")
+    
+    st.write("")
+    
+    st.write("Page 1: Analyze Economic Crisis")
+    
+    st.write("")
+    
+    st.write("Page 2: Check Credit Card Eligibility")
+    
+    st.write("")
+    
+    st.write("Page 3: Check Students Loan Eligibility")
+    
+    st.write("")
+    
+    st.write("Page 4: Check Employee Loan Eligibility")
+    
+    
+
+    
+    
